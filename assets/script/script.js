c31bd1c55aa0b162d902df3ea8cc968907cf6902
helciodev <helcioandre@live.com>
2020-10-28 18:59:48 +0100

    finishes navigation

--- assets/script/script.js
+++ assets/script/script.js
@@ -32,3 +32,23 @@ window.addEventListener('scroll',function () {
   window.scrollY >= 209? fixedNavbar.style.display = 'flex': fixedNavbar.style.display ='none'
 
 } ) 
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.querySelector(".more-stories-small");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+
+  if(window.scrollY >= 4750) navbar.classList.remove('sticky');
+}
